128b695b42e50293832a43855952f068b195b9e4
ggombas36 <ggomba88@gmail.com>
2022-10-09 12:48:23 +0200

    A kép szürkeárnyalatos verziója

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import cv2
+import numpy as np
+
+
+coin = cv2.imread('Images/forintok.jpg', 1)
+cv2.imshow("kep", coin)
+cv2.waitKey(0)
+gray = cv2.imread('Images/forintok.jpg', 0)
+
+
+"""
+gray1 = cv2.cvtColor(coins, cv2.COLOR_BGR2GRAY)
+cv2.imshow("kep3", gray1)
+cv2.waitKey(0)
+"""
